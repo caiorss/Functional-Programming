cc266c1e733ecde5e1c8c1155ed9fc6ff2ab9e59
CaioRS <caiorss.rodrigues@gmail.com>
2015-02-10 11:47:40 -0300

    Changed recursive.hs

--- recursion.hs
+++ recursion.hs
@@ -1,5 +1,20 @@
+{-
+    recursion.hs    
+    Recursive Functions Examples
+    
+-}
 
-
+-- Product of A List
 prod :: [Int] -> Int
-prod [x] = x
+prod [] = 1
 prod (x:xs) = x * prod xs
+
+-- Factorial
+fact 0 = 1
+fact n = n*fact(n-1)
+
+-- Fibbonaci
+fib 0 = 1
+fib 1 = 1
+fib n | n>= 2
+    = fib(n-1) + fib(n-2)
