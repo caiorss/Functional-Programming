7e86aabaf05bdcf3325bb8b247f51c27d63be796
CaioRS <caiorss.rodrigues@gmail.com>
2015-02-11 18:15:43 -0300

    rootsolving.hs - Added new root solving methods

--- rootsolving.hs
+++ rootsolving.hs
@@ -9,14 +9,55 @@ Solves equation using the Newton Method
 (|>) x f = f x
 (|>>) x f = map f x
 
+
+{-
+
+
+*Main> let f x  =  exp(-x) -3*log(x)
+*Main> bissection_solver 1e-5  100 f 0.1 3
+(1.1154491424560549,3.395150475205e-6,18)
+*Main> 
+
+-}
+
+bissection_iterator :: (Floating a, Floating a1, Ord a1) => (a -> a1) -> [a] -> [a]
+bissection_iterator f guesslist = newguess
+    where
+    a =  guesslist !! 0
+    b =  guesslist !! 1
+    c = (a+b)/2.0
+    p = f(a)*f(c)
+    newguess = (\p -> if p < 0.0 then [a, c] else [c, b] ) p
+
+
+bissectionSolver eps itmax f x1 x2 = (root, error, iterations) 
+    where  
+    
+    bissection_error xlist = abs(f $ xlist !! 1)
+    check_error xlist = bissection_error xlist > eps
+
+    iterator = bissection_iterator  f
+
+    rootlist = [x1, x2] |> iterate iterator |> takeWhile check_error |> take itmax
+
+    pair = last rootlist |> iterator
+    root = last pair
+    error = bissection_error pair
+
+    iterations = length rootlist    
+
+-- let f x  =  exp(-x) -3*log(x)
+-- let itf  = bissection_iterator f     
+
+
 secant_iterator :: Floating t => (t -> t) -> [t] -> [t]
 secant_iterator f guesslist = [x, xnext]
     where
-    x =  guesslist !! (length guesslist - 1)
-    x_ = guesslist !! (length guesslist - 2)
+    x =  guesslist !! 0
+    x_ = guesslist !! 1
     xnext = x - f(x)*(x-x_)/(f(x) - f(x_))
 
-secant_solver eps itmax f x1 x2 = (root, error, iterations) 
+secantSolver eps itmax f x1 x2 = (root, error, iterations) 
     where  
     
     secant_error xlist = abs(f $ xlist !! 1)
@@ -24,19 +65,13 @@ secant_solver eps itmax f x1 x2 = (root, error, iterations)
 
     iterator = secant_iterator  f
 
-    rootlist = xlist |> iterate iterator |> takeWhile check_error |> take itmax
+    rootlist = [x1, x2] |> iterate iterator |> takeWhile check_error |> take itmax
 
     pair = last rootlist |> iterator
     root = last pair
     error = secant_error pair
 
     iterations = length rootlist
-
-
-xlist = [1.0, 1.4]
-
-f :: Floating a => a -> a
-f x = x^2 - 2.0
 
 
 {-
@@ -77,6 +112,37 @@ newtonSolver eps itmax f df guess = (root, error, iterations)
     iterations = length rootlist
 
 
+
+
+{-
+
+*Main> let f x = exp(x) - 3.0*x^2
+*Main> 
+*Main> steffensenSolver 1e-5 100 f 4
+(3.733079203355024,3.3912123882373635e-6,54)
+*Main> 
+
+*Main> f 3.733079203355024
+3.3912123882373635e-6
+*Main
+
+-}
+steffensen_iterator :: (Floating a) => (a -> a) -> a -> a
+steffensen_iterator f x = x - y^2/( f(x+y) -f(x))
+    where
+    y = f x
+
+steffensenSolver :: (Floating t, Ord t) => t -> Int -> (t -> t) -> t -> (t, t, Int)
+steffensenSolver eps itmax f guess = (root, error, iterations)
+    where
+    check_root x = abs(f(x)) > eps                                  
+    iterator = steffensen_iterator f    -- Builds the Newton Iterator                              
+    generator = iterate iterator    -- Infinite List that will that holds the roots (Lazy Evaluation)
+
+    rootlist = take itmax $ takeWhile check_root $ generator guess                                  
+    root = iterator $ last $ rootlist                                  
+    error = abs(f(root))
+    iterations = length rootlist
 
 
 
